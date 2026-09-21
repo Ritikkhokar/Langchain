4f4865c4cadff0e145a7638f209264dac9bdd0de
Ritik Khokar <khokharritik1999@gmail.com>
2026-09-21 12:04:20 -0500

    conditional  workflows using langraph

--- chatModel/conditionalLangraphWorkflow.py
+++ chatModel/conditionalLangraphWorkflow.py
@@ -20,9 +20,20 @@ class ReviewState(TypedDict):
 
 
 # ── Nodes ───────────────────────────────────────────────────────
+def findSentiment(state: ReviewState):
+    prompt = f"Analyze the sentiment of the following review. Is it positive or negative? Review: {state['review']}"
 
+    output = model.invoke(prompt, output_parser=str_parser)
+
+    return {"sentiment": output.strip().lower()}
 
 # ── Graph ───────────────────────────────────────────────────────
+graph = StateGraph(ReviewState)
+graph.add_node('find_sentiment', findSentiment)
+graph.add_node('run_diagnosis', runDiagnosis)
+graph.add_node('negative_response', negativeResponse)
+graph.add_node('positive_response', positiveResponse)
+
 
 
 # ── Run ─────────────────────────────────────────────────────────
